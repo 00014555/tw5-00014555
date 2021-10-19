c79410ac1c68e700d233c944a00a0823d4c8e831
00014555 <wiut00014555@gmail.com>
2021-10-19 15:02:56 +0500

    second

---
+++ main.py
@@ -0,0 +1,75 @@
+n=6;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+
+
+# n=10;
+# for i in range(n):
+#     for j in range(i):
+#         print ('* ', end="")
+#     print('')
+
+
+# color_list = ["Red","Green","White","Black"]
+# print(color_list[0])
+# print(color_list[-1])
+
+
+
+# for x in range(6):
+#     print(x)
+
+# for x in range(10, 20, 4):
+#     print(x)
+
+# secret = 7
+# while True:
+#     line = input("Enter your guess : ")
+#     if int(line) == secret:
+#         print("Found it")
+#         break
+#     print(line)
+# print('Done!')
+
+
+
+
+
+
+
+
+# i = 1
+# while i <=10:
+#  print (i)
+#  i += 1
+#
+#
+#
+#
+# i = 1
+# while i <=10:
+#  print (i*"*")
+#  i += 1
+#
+#
+# i = 10
+# while i >=1:
+#  print (i*"*")
+#  i -= 1
+
+
+
+
+
+
+
+
+
+
+# cheeses = ["gauda", "mozzarella", "edam"]
+# print(cheeses[0])
+# print(cheeses[-1])
+# print("brie" in cheeses)
